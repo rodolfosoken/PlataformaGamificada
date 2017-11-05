27a83fb9c7a3c407b2ce478cdee7872037658de9
Rodolfo Soken <rodolfosoken@gmail.com>
2017-11-05 02:46:04 -0200

    mecanismo de busca implementado

--- js/script.js
+++ js/script.js
@@ -1,56 +1,71 @@
 class Aluno {
-	constructor(nome){
-		this.nome = nome;
-	}
+    constructor(nome) {
+        this.nome = nome;
+    }
 
-	toString(){
-		var stringAluno = "<div class=\"row align-items-center rowAluno\">"+
-                    "<div class=\"col-1\">"+
-                        "<img src=\"images/icon-user.png\">"+
-                   " </div> <div class=\"col nome\">"+
-                        "Nome: "+this.nome+" "+
-                    "</div>"+
-                    "<div class=\"col-2\">"+
-                        "<img src=\"images/trofeu-01.png\" class=\"insignia\">"+
-                   " </div>"+
-                    "<div class=\"col-2\">"+
-                        "<img src=\"images/medalhaVermelha.png\" class=\"insignia\">"+
-                   " </div>"+
-                "</div>";
+    toHtml() {
+        var stringAluno = "<div class=\"row align-items-center rowAluno\">" +
+            "<div class=\"col-1\">" +
+            "<img src=\"images/icon-user.png\">" +
+            " </div> <div class=\"col nome\">" +
+            "Nome: " + this.nome + " " +
+            "</div>" +
+            "<div class=\"col-2\">" +
+            "<img src=\"images/trofeu-01.png\" class=\"insignia\">" +
+            " </div>" +
+            "<div class=\"col-2\">" +
+            "<img src=\"images/medalhaVermelha.png\" class=\"insignia\">" +
+            " </div>" +
+            "</div>";
         return stringAluno;
-	}
+    }
 }
 
 var alunos = [];
 
-$(document).ready(function(){
-	
-	//teste que preenche de forma dinamica o quadro de alunos
-	for (var i = 1 ; i <= 5; i++) {
-		var aluno = new Aluno("Aluno "+i);
-		alunos.push(aluno);
-		$("#quadroDeAlunos").append(aluno.toString());
-	}//fim do for
-	
-	//mecanismo de busca, apenas verifica se o nome consta na lista
-	//============= nao foi terminado ========
-	$("#procurarAluno").click(function(){
-		var nomeInserido = $("#nomePesquisa").val();
-		var nomes = document.getElementsByClassName("nome");
-		var naoEncontrado = true;
+$(document).ready(function() {
+	//reseta campo de pesquisa
+	$("#nomePesquisa").val("");
 
-		for (var i = 0; i < nomes.length; i++) {
-			if (nomes[i].innerHTML.includes(nomeInserido)){
-				alert("Nome Encontrado em Aluno nº : "+i);
-				encontrado = !naoEncontrado;
-			} 
-		}
-		if (naoEncontrado) {
-			alert("Não foi possível encotrar o seu nome");
-		}
+    //teste que preenche de forma dinamica o quadro de alunos
+    for (var i = 1; i <= 5; i++) {
+        var aluno = new Aluno("Aluno " + i);
+        alunos.push(aluno);
+        $("#quadroDeAlunos").append(aluno.toHtml());
+    } //fim do for
 
-	});
+    //mecanismo de busca
+    $("#procurarAluno").click(function() {
 
-	//faz a animacao das insignias
-	$(".insignia").addClass("animated flip");
+        var nomeInserido = $("#nomePesquisa").val();
+        var naoEncontrado = true;
+
+        if (nomeInserido) {
+        	alunosAchados = []
+            var nomes = document.getElementsByClassName("nome");
+
+            for (var i = 0; i < alunos.length; i++) {
+                if (alunos[i].nome.includes(nomeInserido)) {
+                	alunosAchados.push(alunos[i]);
+                    naoEncontrado = false;
+                }
+            }
+
+            if (naoEncontrado) {
+                alert("Não foi possível encotrar o seu nome");
+            }else{
+            	var htmlString = "";
+            	for (var i = 0; i < alunosAchados.length; i++) 
+            		htmlString += alunosAchados[i].toHtml();  
+            	$("#quadroDeAlunos").html(htmlString);
+            }
+
+        } else {
+            alert("O nome não pode ser vazio.");
+        }
+
+    });
+
+    //faz a animacao das insignias
+    $(".insignia").addClass("animated flip");
 });
