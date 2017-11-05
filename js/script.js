19dca6fefd2ffaed6fd1929e6ea0ca46846b8868
Rodolfo Soken <rodolfosoken@gmail.com>
2017-11-05 03:28:02 -0200

    classe insignia implementada

--- js/script.js
+++ js/script.js
@@ -1,69 +1,100 @@
 class Aluno {
-    constructor(nome) {
+    constructor(nome, insignias) {
         this.nome = nome;
+        this.insignias = insignias;
     }
 
     toHtml() {
+    	var insigniasHtml = "";
+    	
+    	for (var i = 0; i < this.insignias.length; i++)
+    		insigniasHtml += this.insignias[i].toHtml();
+    	
         var stringAluno = "<div class=\"row align-items-center rowAluno\">" +
             "<div class=\"col-1\">" +
             "<img src=\"images/icon-user.png\">" +
             " </div> <div class=\"col nome\">" +
             "Nome: " + this.nome + " " +
             "</div>" +
-            "<div class=\"col-2\">" +
-            "<img src=\"images/trofeu-01.png\" class=\"insignia\">" +
-            " </div>" +
-            "<div class=\"col-2\">" +
-            "<img src=\"images/medalhaVermelha.png\" class=\"insignia\">" +
-            " </div>" +
+            insigniasHtml+
             "</div>";
         return stringAluno;
     }
 }
 
+class Insignia{
+	constructor(nameFile){
+		this.nameFile = nameFile;
+	}
+
+	toHtml(){
+			return "<div class=\"col-2\">" +
+            "<img src=\"images/"+this.nameFile+"\" class=\"insignia\">" +
+            " </div>"
+	}
+}
+
 var alunos = [];
 
-$(document).ready(function() {
-	//reseta campo de pesquisa
-	$("#nomePesquisa").val("");
+//procura o nome do aluno inserido no campo de busca
+function procurarAluno() {
 
-    //teste que preenche de forma dinamica o quadro de alunos
+    var nomeInserido = $("#nomePesquisa").val();
+    var naoEncontrado = true;
+
+    if (nomeInserido) {
+        alunosAchados = []
+        var nomes = document.getElementsByClassName("nome");
+
+        for (var i = 0; i < alunos.length; i++) {
+            if (alunos[i].nome.includes(nomeInserido)) {
+                alunosAchados.push(alunos[i]);
+                naoEncontrado = false;
+            }
+        }
+
+        if (naoEncontrado) {
+            alert("Não foi possível encotrar o seu nome");
+        } else {
+            var htmlString = "";
+            for (var i = 0; i < alunosAchados.length; i++)
+                htmlString += alunosAchados[i].toHtml();
+            $("#quadroDeAlunos").html(htmlString);
+        }
+
+    } else {
+        alert("O nome não pode ser vazio.");
+    }
+
+}
+
+
+//executa os metodos após o carregamento da página
+$(document).ready(function() {
+    //reseta campo de pesquisa
+    $("#nomePesquisa").val("");
+
+    var insigniaVermelha = new Insignia("medalhaVermelha.png");
+    var insigniaColorida = new Insignia("medalhaColorida.png");
+    var insigniaTrofeu = new Insignia("trofeu-01.png");
+
+    //preenche o quadro de alunos
     for (var i = 1; i <= 5; i++) {
-        var aluno = new Aluno("Aluno " + i);
+        var aluno = new Aluno("Aluno " + i, [insigniaTrofeu, insigniaColorida, insigniaVermelha]);
         alunos.push(aluno);
         $("#quadroDeAlunos").append(aluno.toHtml());
     } //fim do for
 
     //mecanismo de busca
     $("#procurarAluno").click(function() {
+        procurarAluno();
+    });
 
-        var nomeInserido = $("#nomePesquisa").val();
-        var naoEncontrado = true;
+    $("input").keydown(function(e) {
+        if (e.which == 13) {
+            $("#procurarAluno").click(procurarAluno());
 
-        if (nomeInserido) {
-        	alunosAchados = []
-            var nomes = document.getElementsByClassName("nome");
-
-            for (var i = 0; i < alunos.length; i++) {
-                if (alunos[i].nome.includes(nomeInserido)) {
-                	alunosAchados.push(alunos[i]);
-                    naoEncontrado = false;
-                }
-            }
-
-            if (naoEncontrado) {
-                alert("Não foi possível encotrar o seu nome");
-            }else{
-            	var htmlString = "";
-            	for (var i = 0; i < alunosAchados.length; i++) 
-            		htmlString += alunosAchados[i].toHtml();  
-            	$("#quadroDeAlunos").html(htmlString);
-            }
-
-        } else {
-            alert("O nome não pode ser vazio.");
         }
-
     });
 
     //faz a animacao das insignias
