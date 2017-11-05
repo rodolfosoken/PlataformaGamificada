943f993bf9d1169443c644787fc6c6ec3fa4c52e
Rodolfo Soken <rodolfosoken@gmail.com>
2017-11-05 00:51:16 -0200

    primeiro teste com insercao no quadro de alunos: ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	for (var i = 0 ; i < 5; i++) {
+		$("#quadroDeAlunos").append(
+		"<div class=\"row align-items-center rowAluno\">"+
+                    "<div class=\"col\">"+
+                        "<img src=\"images/icon-user.png\">"+
+                   " </div> <div class=\"col\">"+
+                        "Nome: Aluno "+i+" do Quadro "+
+                    "</div>"+
+                    "<div class=\"col-2\">"+
+                        "<img src=\"images/trofeu-01.png\" class=\"insignia\">"+
+                   " </div>"+
+                    "<div class=\"col-2\">"+
+                        "<img src=\"images/medalhaVermelha.png\" class=\"insignia\">"+
+                   " </div>"+
+                "</div>"
+
+		);
+	}
 	
 	$(".insignia").addClass("animated flip");
 });
